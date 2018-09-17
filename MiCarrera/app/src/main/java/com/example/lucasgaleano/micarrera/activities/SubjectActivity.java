f5c59f3934ebe198a64448a5e79b0a80f6bddc10
LucasGaleano <l.a.galeano2@gmail.com>
2018-09-17 20:35:14 -0300

    arreglo bug

--- MiCarrera/app/src/main/java/com/example/lucasgaleano/micarrera/activities/SubjectActivity.java
+++ MiCarrera/app/src/main/java/com/example/lucasgaleano/micarrera/activities/SubjectActivity.java
@@ -1,21 +1,26 @@
 package com.example.lucasgaleano.micarrera.activities;
 
-import android.arch.lifecycle.ViewModelProviders;
+
+import android.arch.lifecycle.Observer;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.text.BoringLayout;
-import android.view.View;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import com.example.lucasgaleano.micarrera.database.Exam;
 import com.example.lucasgaleano.micarrera.database.Repository;
 import com.example.lucasgaleano.micarrera.database.Subject;
-import com.example.lucasgaleano.micarrera.view.SubjectTreeModel;
-import com.example.lucasgaleano.micarrera.view.SubjectView;
 import com.example.lucasgaleano.micarrera.R;
 
-public class SubjectActivity extends Activity {
+import java.util.List;
+
+public class SubjectActivity extends AppCompatActivity {
 
 
     private TextView mtv;
@@ -55,6 +60,17 @@ public class SubjectActivity extends Activity {
                     sub.setState(getResources().getInteger(R.integer.HABILITADA));
                 repo.updateSubject(sub);
 
+            }
+        });
+
+        repo.getAllExam().observe(this, new Observer<List<Exam>>() {
+            @Override
+            public void onChanged(@Nullable final List<Exam> exams) {
+                for (Exam exam : exams) {
+                    Log.d("examen nombre: ", exam.getSubject());
+                    Log.d("examen nota: ", String.valueOf(exam.getScore()));
+                    Log.d("examen fecha: ", exam.getDate().toString());
+                }
             }
         });
 
